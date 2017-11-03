5574858cb933c7531e541fb4c6d0c9f514158aa4
John Jermanis <33067902+jjermanis@users.noreply.github.com>
2017-11-03 10:28:53 -0700

    Problems 16 to 20 (#6)
    * Problems 16 & 17
    
    * Solutiuons for 18 and 19
    
    * Solution for #20

--- unit-tests/Solutions011to020Tests.cs
+++ unit-tests/Solutions011to020Tests.cs
@@ -14,6 +14,11 @@ namespace unit_tests
             Test(Solutions.Solution013, nameof(Solutions.Solution013), 5537376230L, TestSpeed.Fast);
             Test(Solutions.Solution014, nameof(Solutions.Solution014), 837799, TestSpeed.Normal);
             Test(Solutions.Solution015, nameof(Solutions.Solution015), 137846528820L, TestSpeed.Fast);
+            Test(Solutions.Solution016, nameof(Solutions.Solution016), 1366, TestSpeed.Fast);
+            Test(Solutions.Solution017, nameof(Solutions.Solution017), 21124, TestSpeed.Fast);
+            Test(Solutions.Solution018, nameof(Solutions.Solution018), 1074, TestSpeed.Fast);
+            Test(Solutions.Solution019, nameof(Solutions.Solution019), 171, TestSpeed.Fast);
+            Test(Solutions.Solution020, nameof(Solutions.Solution020), 648, TestSpeed.Fast);
         }
     }
 }
