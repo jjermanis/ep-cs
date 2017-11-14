91a7ac97a0c5ea1695a7163704817fe8b5222ced
John Jermanis <jermanis@yahoo.com>
2017-11-13 16:48:00 -0800

    Problems 21 & 22

---
+++ ep-cs/Solutions021to030.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ep_cs
+{
+    public static partial class Solutions
+    {
+        /// <summary>
+        /// Let d(n) be defined as the sum of proper divisors of n (numbers less than n 
+        /// which divide evenly into n). If d(a) = b and d(b) = a, where a ≠ b, then 
+        /// a and b are an amicable pair and each of a and b are called amicable numbers.
+        ///Evaluate the sum of all the amicable numbers under 10000.
+        /// </summary>
+        public static int Solution021()
+            => AmicableNumberForFactorsSum(10_000);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int AmicableNumberForFactorsSum(int n)
+        {
+            // Calculate all factor sums in range
+            var divisorSums = new int[n];
+            for (var i = 0; i < n; i++)
+                divisorSums[i] = SumProperDivisors(i);
+
+            int result = 0;
+            for (var i = 0; i < n; i++)
+            {
+                var currSum = divisorSums[i];
+                if (currSum < n &&
+                    currSum != i &&                 // Don't count cases where number is its own pair
+                    divisorSums[currSum] == i)
+                    result += i;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Return the sum of all positive divisors of n, which are less than n.
+        /// </summary>
+        private static int SumProperDivisors(int n)
+        {
+            int result = 1; // 1 is always a divisor, but n is not
+            var factor = 2;
+            while (factor*factor<n)
+            {
+                if (n % factor == 0)
+                {
+                    // Add the factor, and its cofactor
+                    result += factor;
+                    result += (n / factor);
+                }
+                factor++;
+            }
+            // Handle the case for a perfect square - it's its own cofactor
+            if (factor * factor == n)
+                result += factor;
+
+            return result;
+        }
+
+        public static int Solution022()
+            => FileNameScore("ep_cs.data.Problem22Names.txt");
+
+        public static int FileNameScore(string nameFile)
+        {
+            var names = ReadStringsFromResource(nameFile);
+            names.Sort();
+            int result = 0;
+            for (var i = 0; i < names.Count; i++)
+                result += NameScore(names[i]) * (i + 1);
+            return result;
+        }
+
+        private static int NameScore(string name)
+        {
+            int result = 0;
+            foreach (var c in name.ToUpper())
+                result += c - 'A' + 1;
+            return result;
+        }
+
+        private static readonly char[] TRIM_CHARS = new char[] { '"' };
+        /// <summary>
+        /// 
+        /// </summary>
+        private static List<string> ReadStringsFromResource(string resourceName)
+        {
+            var result = new List<string>();
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    var rowData = reader.ReadLine();
+                    var words = rowData.Split(',');
+                    foreach (var word in words)
+                        result.Add(word.Trim(TRIM_CHARS));
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ unit-tests/Solutions021to030Tests.cs
@@ -0,0 +1,24 @@
+﻿using ep_cs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace unit_tests
+{
+    [TestClass]
+    public class Solutions021to030Tests : BaseTest
+    {
+        [TestMethod]
+        public void Solutions021to030Test()
+        {
+            Test(Solutions.Solution021, nameof(Solutions.Solution021), 31626, TestSpeed.Fast);
+            Test(Solutions.Solution022, nameof(Solutions.Solution022), 871198282, TestSpeed.Fast);
+            //Test(Solutions.Solution023, nameof(Solutions.Solution023), 4179871, TestSpeed.Fast);
+            //Test(Solutions.Solution024, nameof(Solutions.Solution024), 2783915460, TestSpeed.Normal);
+            //Test(Solutions.Solution025, nameof(Solutions.Solution025), 4782, TestSpeed.Fast);
+            //Test(Solutions.Solution026, nameof(Solutions.Solution026), 983, TestSpeed.Fast);
+            //Test(Solutions.Solution027, nameof(Solutions.Solution027), -59231, TestSpeed.Fast);
+            //Test(Solutions.Solution028, nameof(Solutions.Solution028), 669171001, TestSpeed.Fast);
+            //Test(Solutions.Solution029, nameof(Solutions.Solution029), 9183, TestSpeed.Fast);
+            //Test(Solutions.Solution030, nameof(Solutions.Solution030), 443839, TestSpeed.Fast);
+        }
+    }
+}
