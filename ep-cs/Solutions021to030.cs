7db7d17a8d2c3c8d4313be799c53ba3196f6983f
John Jermanis <33067902+jjermanis@users.noreply.github.com>
2017-11-17 10:53:02 -0800

    Solutions for 26 to 30 (#10)

--- ep-cs/Solutions021to030.cs
+++ ep-cs/Solutions021to030.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -191,6 +192,179 @@ namespace ep_cs
                 if (digits > min_digits-1)
                     return n;
             }
+        }
+
+        public static int Solution026()
+            => LongestRepetend(1000);
+
+        public static int LongestRepetend(int max)
+        {
+            var longest_n = 0;
+            var longest_repetend = 0;
+            BigInteger TEN = 10;
+
+            for (int n = 2; n < max; n++)
+            {
+                // Only check prime numbers.  At worst, the repetend will be as long as one
+                // of its factors.
+                if (IsPrime(n))
+                {
+                    // The repetend length is the smallest x for which 10^x-1 % n is zero.
+                    // TODO: x will always be a factor of n-1... we could check for that
+                    for (int x = 1; x < n; x++)
+                    {
+                        if (BigInteger.ModPow(TEN, x, n).Equals(BigInteger.One))
+                        {
+                            if (x > longest_repetend)
+                            {
+                                longest_repetend = x;
+                                longest_n = n;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            return longest_n;
+        }
+
+        public static int Solution027()
+            => GetQuadraticCoefficientProductWithMostPrimes(-1000, 1000);
+
+        /// <summary>
+        /// The function n^2+an+b can be used to generate primes.  For example,
+        /// n^2+n+41 generates 40 primes, for values n=0 through 39.  This function
+        /// returns the value of a*b, for the values of a and b that generate the most
+        /// prime values starting at n=0.
+        /// </summary>
+        private static int GetQuadraticCoefficientProductWithMostPrimes(int min, int max)
+        {
+            // Precaulate primes.  Primes will be checked many, many times.
+            const int MAX_PRIMES = 1000;   // This is hard-coded for the specific problem
+            var primes = GetPrimes(MAX_PRIMES);
+            var primes_sub_max = primes.Where(x => x < max);  
+
+            var max_n = 0;  // This is the most primes found for any a and b
+            int result = 0; // This is a*b corresponding to the max_n case
+
+            // a must be odd.  If not, n^2+an+b will alternate between odd and even.
+            // b must be prime.  When n=0, n^2+an+b equals b.
+            var a_start = (min % 2 == 0) ? min + 1 : min;
+            for (int a=a_start; a < max; a+=2)
+                foreach (var b in primes_sub_max)
+                {
+                    var n = 0;
+                    while (primes.Contains(n * n + a * n + b))
+                        n++;
+                    if (n > max_n)
+                    {
+                        max_n = n;
+                        result = a * b;
+                    }
+                }
+            return result;
+        }
+
+        public static HashSet<int> GetPrimes(int max)
+        {
+            var isComposites = new bool[max + 1];
+
+            var curr_prime = 2;
+            while (curr_prime * curr_prime <= max)
+            {
+                for (var curr = 2 * curr_prime; curr <= max; curr += curr_prime)
+                    isComposites[curr] = true;
+                curr_prime++;
+                while (isComposites[curr_prime])
+                    curr_prime++;
+            }
+            // TODO: is there an optimization if we know the size here?
+            var result = new HashSet<int>();
+            for (int x = 2; x <= max; x++)
+                if (!isComposites[x])
+                    result.Add(x);
+            return result;
+        }
+
+        public static int Solution028()
+            => SpiralDiagonalSum(1001);
+
+        /// <summary>
+        /// A spiral can be formed by adding sequential numbers to a grid in
+        /// clockwise order.  Here is a spiral where size = 5:
+        /// 21  22  23  24  25
+        /// 20   7   8   9  10
+        /// 19   6   1   2  11
+        /// 18   5   4   3  12
+        /// 17  16  15  14  13
+        /// This function returns the sum of the numbers on the diagonals.  In the 
+        /// above grid, this would return 101.
+        /// </summary>
+        public static int SpiralDiagonalSum(int size)
+        {
+            var result = 1;
+            for (var curr = 3; curr <= size; curr += 2)
+            {
+                // For the diagonal going up and to the righ, the numbers are the squares
+                // of odd numbers (e.g. 1, 9, 25, 49, etc.).  The other three numbers are
+                // each (n-1) from the prior number.  The following is the sum of the four
+                // "corners" at a specific grid size.
+                result += (4 * curr * curr) - (6 * curr) + 6; 
+            }
+            return result;
+        }
+
+        public static int Solution029()
+            => DistinctPowerTerms(100, 100);
+
+        public static int DistinctPowerTerms(int max_a, int max_b)
+        {
+            var terms = new HashSet<BigInteger>();
+
+            // There are probably clever ways to do this counting factors, but this is
+            // blazing fast to solve via brute force (actually calculating the powers)
+            // for the problem as stated.
+            for (var a=2; a<=max_a; a++)
+                for (var b=2; b<=max_b; b++)
+                    terms.Add(BigInteger.Pow(a, b));
+            return terms.Count;
+        }
+
+        public static int Solution030()
+            => SumNumbersThatAreSumOfPowers(5);
+
+        public static int SumNumbersThatAreSumOfPowers(int exp)
+        {
+            // Check for wold-be overflow
+            if (exp > 8)
+                throw new ArgumentException("Max supported exp: 8");
+
+            int pow(int x, int y)
+            {
+                var n = 1;
+                for (var i = 0; i < y; i++)
+                    n *= x;
+                return n;
+            }
+
+            var max = exp * pow(9, exp);
+
+            var result = 0;
+            for (var num=0; num<=max; num++)
+            {
+                var sum = 0;
+                var curr = num;
+                while (curr > 0)
+                {
+                    sum += pow(curr % 10, exp);
+                    curr /= 10;
+                }
+                if (sum == num)
+                    result += num;
+            }
+            // Cancel out 1, which will always be included, but the problem statement
+            // says not to, since it is not a sum.
+            return result - 1;
         }
     }
 }
